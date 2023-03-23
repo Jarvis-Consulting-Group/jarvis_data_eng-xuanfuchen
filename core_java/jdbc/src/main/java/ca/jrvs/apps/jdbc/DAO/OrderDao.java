6bc62d18b61994ce93b959314e1f992d42681b25
Xuanfu Chen <sean.xfc@gmail.com>
2023-03-23 22:41:06 +0000

    add findById & getOrdersByCustomer feature to OrderDao

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/DAO/OrderDao.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/DAO/OrderDao.java
@@ -36,6 +36,8 @@ public class OrderDao extends DataAccessObject<OrderDto> {
             "   JOIN order_item ol ON ol.order_id = o.order_id " +
             "   JOIN product p ON ol.product_id = p.product_id " +
             "WHERE o.order_id = ?;";
+
+    private static final String GET_BY_CUSTOMER = "SELECT * FROM get_orders_by_customer(?)";
     public OrderDao(Connection connection) {
         super(connection);
     }
@@ -44,9 +46,13 @@ public class OrderDao extends DataAccessObject<OrderDto> {
     public OrderDto findById(long id) {
         OrderDto order = null;
         try(PreparedStatement statement = connection.prepareStatement(GET_BY_ID)){
+            //finish the statement by adding the argument into it
             statement.setLong(1, id);
+            //get the result set from the execution result
             ResultSet rs = statement.executeQuery();
+            //iterate through all retrieved records(there should be only one records)
             while(rs.next()){
+                //create an orderDto object if it has not been created
                 if(order == null){
                     order = new OrderDto();
                     order.setId(rs.getLong("order_id"));
@@ -61,6 +67,7 @@ public class OrderDao extends DataAccessObject<OrderDto> {
                     order.setSalespersonEmail(rs.getString("salesperson_email"));
                     order.setOrderLines(new ArrayList<>());
                 }
+                //add all orderlines to the orderline list
                 OrderLine orderLine = new OrderLine();
                 orderLine.setQuantity(rs.getInt("quantity"));
                 orderLine.setProductCode(rs.getString("code"));
@@ -94,6 +101,52 @@ public class OrderDao extends DataAccessObject<OrderDto> {
 
     @Override
     public void delete(long id) {
+    }
 
+    public List<OrderDto> getOrdersByCustomer(long customerId){
+        List<OrderDto> orders = new ArrayList<>();
+        try(PreparedStatement statement = this.connection.prepareStatement(GET_BY_CUSTOMER)){
+            //finished the statement by adding the argument into it
+            statement.setLong(1, customerId);
+            //get the result set form the execution result.
+            ResultSet rs = statement.executeQuery();
+            OrderDto order = null;
+            long prevOrderId = 0;
+            while(rs.next()){
+                long currOrderId = rs.getLong("order_id");
+                //if the order id of current record is not the same as the previous record,
+                //then we need to create a new order object
+                if(currOrderId != prevOrderId){
+                    order = new OrderDto();
+                    orders.add(order);
+                    order.setId(currOrderId);
+                    order.setCustomerFirstName(rs.getString("cust_first_name"));
+                    order.setCustomerLastName(rs.getString("cust_last_name"));
+                    order.setCustomerEmail(rs.getString("cust_email"));
+                    order.setCreationDate(rs.getDate("creation_dt"));
+                    order.setTotalDue(rs.getDouble("total_due"));
+                    order.setStatus(rs.getString("status"));
+                    order.setSalespersonFirstName(rs.getString("sales_first_name"));
+                    order.setSalespersonLastName(rs.getString("sales_last_name"));
+                    order.setSalespersonEmail(rs.getString("sales_email"));
+                    order.setOrderLines(new ArrayList<>());
+                }
+                //build a new orderline object and add it to the orderline list
+                OrderLine orderLine = new OrderLine();
+                orderLine.setQuantity(rs.getInt("item_quanitty"));
+                orderLine.setProductCode(rs.getString("item_code"));
+                orderLine.setProductName(rs.getString("item_name"));
+                orderLine.setProductSize(rs.getInt("item_size"));
+                orderLine.setProductVariety(rs.getString("item_variety"));
+                orderLine.setProductPrice(rs.getDouble("item_price"));
+                order.getOrderLines().add(orderLine);
+                //in the next iteration, current ID will become the previous ID
+                prevOrderId = currOrderId;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return orders;
     }
 }
--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -2,10 +2,12 @@ package ca.jrvs.apps.jdbc;
 
 import ca.jrvs.apps.jdbc.DAO.CustomerDao;
 import ca.jrvs.apps.jdbc.DAO.OrderDao;
+import ca.jrvs.apps.jdbc.DTO.OrderDto;
 import ca.jrvs.apps.jdbc.PO.Customer;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 public class JDBCExecutor {
     public static void main(String[] args) {
@@ -19,7 +21,10 @@ public class JDBCExecutor {
         try{
             Connection connection = dcm.getConnection();
             OrderDao orderDao = new OrderDao(connection);
-            System.out.println(orderDao.findById(1000).toString());
+            List<OrderDto> orders = orderDao.getOrdersByCustomer(789);
+            orders.forEach(System.out::println);
+            System.out.println(orderDao.findById(1009).toString().equals(orders.get(0).toString()));
+            System.out.println(orderDao.findById(1064).toString().equals(orders.get(1).toString()));
         }catch(SQLException e){
             e.printStackTrace();
         }
