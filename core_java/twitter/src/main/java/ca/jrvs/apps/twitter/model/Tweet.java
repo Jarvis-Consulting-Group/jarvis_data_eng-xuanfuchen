ab4852f8d5e10b3b08ad3e16ce22cb08a11ff18d
Xuanfu Chen <41319054+xuanfuchen@users.noreply.github.com>
2023-04-17 15:40:39 -0400

    Add Property "Includes" to Tweet model

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
@@ -5,9 +5,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Tweet {
     private Data data;
+    private Includes includes;
 
     public Tweet() {
         this.data = new Data();
+        this.includes = new Includes();
     }
 
     public Tweet(Data data) {
@@ -20,6 +22,14 @@ public class Tweet {
 
     public void setData(Data data) {
         this.data = data;
+    }
+
+    public Includes getIncludes() {
+        return includes;
+    }
+
+    public void setIncludes(Includes includes) {
+        this.includes = includes;
     }
 
     @Override
