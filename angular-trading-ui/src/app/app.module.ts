f9276b4811abbf2ce0bd2921edbf00577532f4e3
Xuanfu Chen <41319054+xuanfuchen@users.noreply.github.com>
2023-05-12 16:12:16 -0400

    recreate project

--- angular-trading-ui/src/app/app.module.ts
+++ angular-trading-ui/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
