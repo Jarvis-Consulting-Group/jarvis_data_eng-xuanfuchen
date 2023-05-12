b9362ae5027ec10748bed8dc922c2e9c590fa628
ubuntu <ubuntu@JRD.myguest.virtualbox.org>
2023-05-12 14:41:42 -0400

    add navbar

--- angular-trading-ui/src/app/app.module.ts
+++ angular-trading-ui/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
